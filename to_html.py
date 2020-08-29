9f4b725f314040e1c79ce907050b6b44cce0d10d
igor.b <igor.brevdo@gmail.com>
2020-08-29 22:09:59 +0300

    update css file path

--- to_html.py
+++ to_html.py
@@ -16,7 +16,7 @@ TEMPLATE="""
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
     <title>{{title}}</title>
-    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="https://ibrevdo.github.io/notes/css/common.css">
 </head>
 
 <body>
